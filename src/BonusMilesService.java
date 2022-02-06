07129dfcc84691355318b8e2f501c8591460a404
GrigoryCheremin <Greenx.git.work@gmail.com>
2022-02-06 08:31:18 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate (int cost){
+        int price = 20; // цена бонусной мили, руб.
+        int miles = cost/price;
+        System.out.println("Количество бонусных миль:");
+        return miles;
+    }
+}
